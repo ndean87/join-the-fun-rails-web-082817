e1b7301fab165ee8893bcb75be84aaa2475e7f5b
ndean87 <ndean87@gmail.com>
2017-09-26 11:10:22 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,8 @@
 class Ride < ActiveRecord::Base
+
+  belongs_to :taxi
+  belongs_to :passenger
+
+
+
 end
--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,2 +1,18 @@
 class Taxi < ActiveRecord::Base
+
+  has_many :passengers, through: :rides
+  has_many :rides
+
 end
+
+
+# 
+#   has_many :stalker_users, class_name: "FollowerUser", foreign_key: :follower_id
+#   has_many :idol_users, class_name: "FollowerUser", foreign_key: :idol_id
+#   has_many :followers, through: :stalker_users
+#   has_many :idols, through :idol_users
+# end
+# class FollowerUser
+#   belongs_to :follower, class_name: "User"
+#   belongs_to :idol, class_name: "User"
+# end
